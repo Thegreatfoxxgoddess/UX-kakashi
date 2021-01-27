9043b48f6641ede3c4606ce81b4387732f7efcd7
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2021-01-27 12:04:29 -0300

    Update romaji.py

--- userge/plugins/romaji.py
+++ userge/plugins/romaji.py
@@ -1,3 +1,7 @@
+## Creator @midnightmadwalk to be found on tg
+## on github as https://github.com/iMBadBoi
+## i just improvised it a lil'...phew..
+
 from google_trans_new import google_translator
 from userge import Message, userge
 
@@ -13,7 +17,11 @@ translator = google_translator()
     },
 )
 async def romaji_(message: Message):
-    x = str(message.input_str or message.reply_to_message.text)
+    x = str(
+        message.input_str
+        or message.reply_to_message.text
+        or message.reply_to_message.caption
+    )
     if not x:
         await message.err("No Input Found")
     else:
